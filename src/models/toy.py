dbd46d89e3f360040fdd2bfe051e13575b484d08
Mtrikkyo <tsubasa.masumura@icoud.com>
2024-04-15 07:52:13 +0000

    Fix: fix flatten layer dim.

--- src/models/toy.py
+++ src/models/toy.py
@@ -8,7 +8,7 @@ class Toymodel(nn.Sequential):
             nn.Conv2d(3, 64, 3, padding=1),
             nn.ReLU(inplace=False),
             nn.AdaptiveAvgPool2d(1),
-            nn.Flatten(0),
+            nn.Flatten(1, -1),
             nn.Linear(64, 182),
         )
 
@@ -17,4 +17,4 @@ if __name__ == "__main__":
     from torchinfo import summary
 
     model = Toymodel()
-    summary(model, input_size=(3, 224, 224))
+    summary(model, input_size=(3, 224, 224), batch_dim=0)
